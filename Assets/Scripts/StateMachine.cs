8540055304c90ecdd0ac57e5ee8f0e60083b644a
skarnat0@email.cpcc.edu <skarnat0@email.cpcc.edu>
2021-09-25 10:15:35 -0400

    updated state machine script

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
+
+    public enum State { 
+    
+        idle,
+        walking,
+        swimming,
+        climbing
+    }
+
+    public State currentState = State.idle;
+    Vector3 lastPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (currentState) {
+            
+            case State.idle: Idle();  break;
+            case State.walking: Walking(); break;
+            case State.climbing: Climbing(); break;
+            case State.swimming: Swimming(); break;
+            default:break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       // Debug.Log(other.name);
+        if (other.name == "WaterTrigger")
+        {
+            currentState = State.swimming;
+        }
+        else if (other.name == "MountainTrigger")
+        {
+            currentState = State.climbing;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        currentState = State.walking;
+    }
+
+    void Swimming()
+    {
+        Debug.Log("I am Swimming");
+    }
+
+    void Climbing()
+    {
+        Debug.Log("I am Climbing");
+    }
+
+    void Idle()
+    {
+        Debug.Log("I am Idle");
+
+        if (lastPosition != transform.position) 
         
+        {
+            currentState = State.walking;
+        }
+        lastPosition = transform.position;
+
+
+    }
+
+    void Walking()
+    {
+        Debug.Log("I am Walking");
+
+        if (lastPosition == transform.position)
+
+        {
+            currentState = State.idle;
+        }
+        lastPosition = transform.position;
     }
 }
